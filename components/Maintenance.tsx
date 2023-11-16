b292d35bd898c2b03c3ca5d082e0b77b31c61c05
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2023-11-16 06:42:04 +0530

    Maintenance mode
    Signed-off-by: Vishal-Marvel <vishal.pvn.edu@gmail.com>

---
+++ components/Maintenance.tsx
@@ -0,0 +1,5 @@
+export const Maintenance = () => {
+    return (<div className={"flex justify-center items-center align-middle h-screen"}>
+        <span className={"font-bold text-2xl"}>This page is under Maintenance, Kindly Check Later</span>
+    </div>)
+}

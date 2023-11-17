a2930e0046eba9572507b11aea564d80fe0018c5
Vishal <77133850+Vishal-Marvel@users.noreply.github.com>
2023-11-17 12:16:49 +0530

    maintenance animation

--- components/Maintenance.tsx
+++ components/Maintenance.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export const Maintenance = () => {
     return (<div className={"flex flex-col justify-center items-center align-middle h-screen text-center"}>
-        <Image src={maintenance} alt={"Maintenance"} height={150} className="animate-spin "/>
+        <Image src={maintenance} alt={"Maintenance"} height={150} className="animate-spin-slow rounded-4xl"/>
         <span className={"font-bold text-2xl text-center"}>This site is under Maintenance, Kindly Check Later</span>
     </div>)
 }
